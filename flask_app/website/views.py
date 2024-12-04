dc16cf117452f84dc65f97e80d6104206d483374
Mert Bozkurtlar <bozkurtlarmert@gmail.com>
2024-12-04 12:22:47 +0300

    Add placeholder pages for rest of the views

--- flask_app/website/views.py
+++ flask_app/website/views.py
@@ -5,3 +5,15 @@ views = Blueprint('views', __name__)
 @views.route('/')
 def homepage():
     return render_template("home.html")
+
+@views.route('/players')
+def players():
+    return render_template("players.html")
+
+@views.route('/teams')
+def teams():
+    return render_template("teams.html")
+
+@views.route('/seasons')
+def seasons():
+    return render_template("seasons.html")

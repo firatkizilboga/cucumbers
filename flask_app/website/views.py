1c1db9ad74a628409ffbcb9e3731f1c5618ae956
replatutum <alpertutum70@gmail.com>
2024-12-08 17:09:44 +0300

    Created templates and view functions for every player table + added dropdown menu to player section in navbar

--- flask_app/website/views.py
+++ flask_app/website/views.py
@@ -10,6 +10,14 @@ def homepage():
 def players():
     return render_template("players.html")
 
+@views.route('/player_season_stats')
+def player_season_stats():
+    return render_template("player_season_stats.html")
+
+@views.route('/player_season_info')
+def player_season_info():
+    return render_template("player_season_info.html")
+
 @views.route('/teams')
 def teams():
     return render_template("teams.html")

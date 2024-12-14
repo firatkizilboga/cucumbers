dba4c6cd6bb3ba897d74330a664e6ac1acff1211
Mert Bozkurtlar <bozkurtlarmert@gmail.com>
2024-12-14 17:57:58 +0300

    Teams ETL

---
+++ database/ETL_queries/teams.sql
@@ -0,0 +1,9 @@
+INSERT INTO Teams (team_name, team_abbreviation) 
+SELECT DISTINCT
+    team,
+    abbreviation
+FROM Team_Abbrev
+WHERE 1=1
+    AND abbreviation IS NOT NULL
+    AND team IS NOT NULL
+    ;

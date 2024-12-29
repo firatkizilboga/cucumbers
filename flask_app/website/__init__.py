2981fb890f3a6f0c5c3ab58b65a95a1e6e7303aa
Fırat Kızılboğa <firatkizilboga11@gmail.com>
2024-12-29 17:37:27 +0300

    Prettier

--- flask_app/website/__init__.py
+++ flask_app/website/__init__.py
@@ -1,12 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-from .views import db, views
+from .views import db, views, cache
 from .auth import auth
+
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'dev secret key'
+    app.config["SECRET_KEY"] = "dev secret key"
 
     # Example MySQL config
     MYSQL_USER = "admin"
@@ -16,25 +17,27 @@ def create_app():
     MYSQL_DATABASE = "cucumdb"
 
     # Configure SQLAlchemy with MySQL
-    app.config['SQLALCHEMY_DATABASE_URI'] = (
+    app.config["SQLALCHEMY_DATABASE_URI"] = (
         f"mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}"
         f"@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DATABASE}"
     )
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
+    app.config["CACHE_TYPE"] = "SimpleCache"
+    app.config["CACHE_DEFAULT_TIMEOUT"] = 300
     db.init_app(app)
-
-
+    cache.init_app(app)
     login_manager = LoginManager()
-    login_manager.login_view = 'auth.login'  # Name of our login route
+    login_manager.login_view = "auth.login"  # Name of our login route
     login_manager.init_app(app)
 
     from .auth import get_user_by_id
+
     @login_manager.user_loader
     def load_user(user_id):
         return get_user_by_id(int(user_id))
 
-    app.register_blueprint(views, url_prefix='/')
-    app.register_blueprint(auth, url_prefix='/')
+    app.register_blueprint(views, url_prefix="/")
+    app.register_blueprint(auth, url_prefix="/")
 
     return app
